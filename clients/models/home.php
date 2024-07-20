17dad99a179b99e92bb31e796a023fe817163bec
Longav <Long1999tb@gmail.com>
2024-07-20 18:26:07 +0700

    Giải quyết xung đột và áp dụng thay đổi từ stash

--- clients/models/home.php
+++ clients/models/home.php
@@ -5,7 +5,8 @@ class Home extends BaseModel  {
     public $tableName = 'products';
 
     public $image = 'images';
-
+    public $color = 'colors';
+public $varient = 'product_details';
     public function allimages($id) {
         try {
             global $coreApp;
@@ -20,6 +21,36 @@ class Home extends BaseModel  {
           
         }
     }
+    public function allvariant($id) {
+        try {
+            global $coreApp;
+            $sql = "SELECT * FROM {$this->varient} WHERE `product_id` = $id ";
+            // var_dump($sql);
+            // die();
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            $coreApp->debug($e);
+          
+        }
+    }
+    public function allcolor() {
+        try {
+            global $coreApp;
+            $sql = "SELECT * FROM {$this->color}";
+            // var_dump($sql);
+            // die();
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            $coreApp->debug($e);
+          
+        }
+    }
+
+
     public function findIdTable($id) {
         try {
             global $coreApp;
